2ab13baf30d5d1cedba51427fdb6157c5a03071f
nurrochman-ramadhon <nurrochman.acc@gmail.com>
2024-06-01 23:10:58 +0700

    feat: fix responsive web

--- src/components/home-page/section-1.jsx
+++ src/components/home-page/section-1.jsx
@@ -3,7 +3,7 @@ import Image from "next/image";
 import { Carousel } from "../shared/carouselSectionOne";
 export const SectionOne = () => {
   return (
-    <div className="h-[500px]">
+    <div className="h-[400px] md:h-[500px] lg:h-[540px]">
         <Carousel/>
     </div>
   );
--- src/components/home-page/section-2.jsx
+++ src/components/home-page/section-2.jsx
@@ -1,7 +1,7 @@
 import { Services } from "../shared/servicesSectionTwo";
 export const SectionTwo = () => {
   return (
-    <div className="mt-8">
+    <div className="mt-8 ">
       <p className=" text-2xl text-center font-semibold mb-8">Layanan Kami</p>
       <Services />
     </div>
